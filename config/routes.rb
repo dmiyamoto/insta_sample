33fb837095c87e5e8e08b0951cb9bcfde09a5c6d
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-09-01 02:13:39 +0900

    insta_sample initial

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: 'posts#index'
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+
+  resources :users, only: %i(show)
+  resources :posts, only: %i(index new create show destroy) do
+    resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy), shallow: true
+    resources :comments, only: %i(index create destroy), shallow: true
+  end
+end
